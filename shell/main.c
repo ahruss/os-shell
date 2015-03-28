0c3fbb934522b8e64f9167d817eccffe0f4eb1b6
Alexander Russ <git@alexanderruss.com>
2015-03-28 04:58:14 -0400

    Initial Commit

---
+++ shell/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  shell
+//
+//  Created by Alexander Russ on 3/28/15.
+//  Copyright (c) 2015 Alexander Russ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
