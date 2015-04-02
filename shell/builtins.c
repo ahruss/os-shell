3b5c0f06beea3cdd8f6dce0802c8dd6554888a75
Eric <epogash@ufl.edu>
2015-04-02 04:02:40 -0400

    Added builtin method skeletons.

--- shell/builtins.c
+++ shell/builtins.c
@@ -1,8 +1,64 @@
 #include "builtins.h"
 #include <stdio.h>
 
-bool isBuiltin(char* command) {
-    return false;
+#define NUMBUILTINS 7
+
+char* builtin_commands[] = {
+    "setenv",
+    "printenv",
+    "unsetenv",
+    "cd",
+    "alias",
+    "unalias",
+    "bye"
+};
+
+int (*builtin_functions[]) (char **) = {
+    &setenv,
+    &printenv,
+    &unsetenv,
+    &cd,
+    &alias,
+    &unalias,
+    &bye
+};
+
+int setenv() {
+    return 1;
+}
+
+int printenv() {
+    return 1;
+}
+
+int unsetenv() {
+    return 1;
+}
+
+int cd() {
+    return 1;
+}
+
+int alias() {
+    return 1;
+}
+
+int unalias() {
+    return 1;
+}
+
+int bye() {
+    return 1;
+}
+
+
+int isBuiltin(char* command) {
+    for(int i = 0; i < NUMBUILTINS; i++) {
+        if(strcmp(command, builtin_commands[i]) == 0) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int executeBuiltin(char* command, StringList* args) {
--- shell/builtins.h
+++ shell/builtins.h
@@ -1,6 +1,16 @@
 #ifndef __OSShell__builtins__
 #define __OSShell__builtins__
 #include "all.h"
-bool isBuiltin(char* command);
+
+int isBuiltin(char* command);
+
+int setenv();
+int printenv();
+int unsetenv();
+int cd();
+int alias();
+int unalias();
+int bye();
+
 int executeBuiltin(char* command, StringList* args);
 #endif /* defined(__OSShell__builtins__) */
