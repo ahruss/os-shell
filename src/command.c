60b123ced02b7098fd3f8f8aaa911d5c2612cf64
Eric <epogash@ufl.edu>
2015-04-09 00:33:20 -0400

    Deleted print statements

--- src/command.c
+++ src/command.c
@@ -131,9 +131,7 @@ pid_t executeCommand(Command c) {
     
     getExecutable(c);
     StringList *tempList = c->args;
-    printf("EXECUTABLE: %s", c->executable);
     while(tempList != NULL) {
-        printf("ARGS: %s", tempList->data);
         tempList = tempList->next;
     }
 
